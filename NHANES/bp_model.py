f1777081b02dd781d88a77d787ee0fd22612e824
Kerby Shedden <kshedden@umich.edu>
2019-09-14 13:45:32 -0400

    update

--- NHANES/bp_model.py
+++ NHANES/bp_model.py
@@ -1,3 +1,21 @@
+# This script uses regression analysis to understand the relationhip
+# between blood pressure (as an outcome) and explanatory factors
+# including age, sex, BMI, and ethnicity.  The analysis is based on
+# fitting linear models using ordinary least squares, with blood
+# pressure as the dependent variable, and the explanatory factors as
+# covariates (independent variables).  All models contain the same
+# four explanatory variables, but the models are parameterized in
+# different ways.  Some models are very simple while others have
+# nonlinear terms and interactions.
+#
+# The different models are assessed by plotting fitted values for
+# various subpopulations.  Since all of the regression functions are
+# well over 3-dimensional, we can plot E[Y|X] against one component of
+# X, while holding the other components of X equal to reference
+# values.  For example, we can plot the fitted mean blood pressure as
+# a function of age, for each sex, for each ethnic group, and for
+# people with average BMI.
+
 import numpy as np
 import statsmodels.api as sm
 from data_prep import dx
@@ -6,8 +24,8 @@ import matplotlib.pyplot as plt
 
 def plot_fit_by_age(result, fml):
 
-    # Create a dataframe in which all variables are at the
-    # reference level
+    # Create a dataframe in which all variables are at the reference
+    # level
     da = dx.iloc[0:100, :].copy()
     da["RIDAGEYR"] = np.linspace(18, 80, 100)
     da["RIDRETH1"] = "OH"
@@ -42,8 +60,8 @@ def plot_fit_by_age(result, fml):
 
 def plot_fit_by_bmi(result, fml):
 
-    # Create a dataframe in which all variables are at the
-    # reference level
+    # Create a dataframe in which all variables are at the reference
+    # level
     da = dx.iloc[0:100, :].copy()
     da["BMXBMI"] = np.linspace(15, 35, 100)
     da["RIDRETH1"] = "OH"
@@ -97,8 +115,8 @@ def plot_basis(xv, xmat, names, degf):
 
 def plot_fit_by_age_race(result, fml):
 
-    # Create a dataframe in which all variables are at the
-    # reference level
+    # Create a dataframe in which all variables are at the reference
+    # level
     da = dx.iloc[0:100, :].copy()
     da["RIDAGEYR"] = np.linspace(18, 80, 100)
     da["BMXBMI"] = 22
@@ -132,21 +150,23 @@ def plot_fit_by_age_race(result, fml):
     pdf.savefig()
 
 
-
 pdf = PdfPages("bp_model.pdf")
 
+# Main effects only (additive, linear model)
 fml0 = "BPXSY1 ~ RIDAGEYR + BMXBMI + Female + RIDRETH1"
 model0 = sm.OLS.from_formula(fml0, data=dx)
 result0 = model0.fit()
 plot_fit_by_age(result0, fml0)
 plot_fit_by_bmi(result0, fml0)
 
+# Interactions but no nonlinearities
 fml1 = "BPXSY1 ~ (RIDAGEYR + BMXBMI) * Female + RIDRETH1"
 model1 = sm.OLS.from_formula(fml1, data=dx)
 result1 = model1.fit()
 plot_fit_by_age(result1, fml1)
 plot_fit_by_bmi(result1, fml1)
 
+# Nonlinearities but no interactions
 fml2 = "BPXSY1 ~ bs(RIDAGEYR, 5) + bs(BMXBMI, 5) + Female + RIDRETH1"
 model2 = sm.OLS.from_formula(fml2, data=dx)
 result2 = model2.fit()
@@ -155,6 +175,8 @@ plot_fit_by_bmi(result2, fml2)
 
 plot_basis(dx.RIDAGEYR.values, result2.model.exog, result2.model.exog_names, 5)
 
+# Use AIC to select the degrees of freedom for age and BMI, each of
+# which is modeled using spline basis functions.
 aic = np.zeros((15, 15)) + np.nan
 for age_df in range(3, 15):
     for bmi_df in range(3, 15):
@@ -163,6 +185,8 @@ for age_df in range(3, 15):
         result3 = model3.fit()
         aic[age_df, bmi_df] = result3.aic
 
+# Make a heatmap of the AIC values, so we can see where the best fit
+# is.
 plt.clf()
 plt.imshow(aic, interpolation="nearest")
 plt.xlabel("BMI df", size=15)
@@ -172,53 +196,64 @@ plt.xlim(3, 14)
 plt.ylim(3, 14)
 pdf.savefig()
 
+# This is the additive spline-based model that is optimal in terms of
+# degrees of freedom, as selected by AIC.
 fml4 = "BPXSY1 ~ bs(RIDAGEYR, 6) + bs(BMXBMI, 5) + Female + RIDRETH1"
 model4 = sm.OLS.from_formula(fml4, data=dx)
 result4 = model4.fit()
 plot_fit_by_age(result4, fml4)
 plot_fit_by_bmi(result4, fml4)
 
+# This is a polynomial model, provided only for comparison
 fml5 = "BPXSY1 ~ RIDAGEYR + I(RIDAGEYR**2) + I(RIDAGEYR**3) + BMXBMI + I(BMXBMI**2) + I(BMXBMI**3) + Female + RIDRETH1"
 model5 = sm.OLS.from_formula(fml5, data=dx)
 result5 = model5.fit()
 plot_fit_by_age(result5, fml5)
 plot_fit_by_bmi(result5, fml5)
 
+# This is another polynomial model
 fml6 = "BPXSY1 ~ RIDAGEYR + I(RIDAGEYR**2) + I(RIDAGEYR**3) + I(RIDAGEYR**4) + I(RIDAGEYR**5) + BMXBMI + I(BMXBMI**2) + I(BMXBMI**3) + I(BMXBMI**4) + I(BMXBMI**5) + Female + RIDRETH1"
 model6 = sm.OLS.from_formula(fml6, data=dx)
 result6 = model6.fit()
 plot_fit_by_age(result6, fml6)
 plot_fit_by_bmi(result6, fml6)
 
+# Take model 5 above and allow the curves to vary by sex
 fml7 = "BPXSY1 ~ (bs(RIDAGEYR, 6) + bs(BMXBMI, 5)) * Female + RIDRETH1"
 model7 = sm.OLS.from_formula(fml7, data=dx)
 result7 = model7.fit()
 plot_fit_by_age(result7, fml7)
 plot_fit_by_bmi(result7, fml7)
 
+# Now include interactions between age and BMI, along with the other
+# interactions, but the role of BMI is conditionally linear
 fml8 = "BPXSY1 ~ bs(RIDAGEYR, 5) * BMXBMI * Female + RIDRETH1"
 model8 = sm.OLS.from_formula(fml8, data=dx)
 result8 = model8.fit()
 plot_fit_by_age(result8, fml8)
 plot_fit_by_bmi(result8, fml8)
 
+# This is similar to the model above, except that BMI is now allowed
+# to be nonlinear
 fml9 = "BPXSY1 ~ bs(RIDAGEYR, 5) * bs(BMXBMI, 4) * Female + RIDRETH1"
 model9 = sm.OLS.from_formula(fml9, data=dx)
 result9 = model9.fit()
 plot_fit_by_age(result9, fml9)
 plot_fit_by_bmi(result9, fml9)
 
+# Model a common nonlinear structure for age and BMI using splines,
+# then allow these curves to be translated
 fml10 = "BPXSY1 ~ bs(RIDAGEYR, 5) + bs(BMXBMI, 4) + Female * RIDRETH1"
 model10 = sm.OLS.from_formula(fml10, data=dx)
 result10 = model10.fit()
 plot_fit_by_age_race(result10, fml10)
 
+# Model common nonlinear curves for BMI and age using splines, and
+# allow these common curves to vary in a linear manner by age gender
+# and ethnicity.
 fml11 = "BPXSY1 ~ bs(BMXBMI, 4) + bs(RIDAGEYR, 5) + RIDAGEYR * Female * RIDRETH1"
 model11 = sm.OLS.from_formula(fml11, data=dx)
 result11 = model11.fit()
 plot_fit_by_age_race(result11, fml11)
 
 pdf.close()
-
-import os
-os.system("cp bp_model.pdf ~kshedden")
--- NHANES/data_prep.py
+++ NHANES/data_prep.py
@@ -1,33 +1,50 @@
-import pandas as pd
-import numpy as np
-
-# Download some of the NHANES data files
+# This script merges a few NHANES files, so that they can then
+# be analyzed, e.g. using regression analysis
+#
+# First, download some of the NHANES data files.  You can do
+# this through the NHANES web site, or on the command line
+# using the commands below:
+#
 # wget https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/DEMO_I.XPT
 # wget https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/BPX_I.XPT
 # wget https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/BMX_I.XPT
+#
+# See the NHANES web site for code books describing what each
+# variable means.
 
+import pandas as pd
+import numpy as np
+
+# List of file names to merge
 xpt_files = ["DEMO_I.XPT", "BPX_I.XPT", "BMX_I.XPT"]
 
+# Retain these variables.
 vars = [
     ["SEQN", "RIAGENDR", "RIDAGEYR", "RIDRETH1"],
     ["SEQN", "BPXSY1", "BPXDI1"],
     ["SEQN", "BMXWT", "BMXHT", "BMXBMI", "BMXLEG", "BMXARMC"],
 ]
 
+# Load each individual file and keep only the variables of interest
 da = []
-
 for idf, fn in enumerate(xpt_files):
-
     df = pd.read_sas(fn)
     df = df.loc[:, vars[idf]]
     da.append(df)
 
-
+# SEQN is a common subject ID that can be used to merge the files.
+# These are cross sectional (wide form) files, so there is at most
+# one row per subject.  Subjects may be missing from a file if they
+# did not participate in those assessments.  All subjects should be
+# present in the demog file.
 dx = pd.merge(da[0], da[1], left_on="SEQN", right_on="SEQN")
 dx = pd.merge(dx, da[2], left_on="SEQN", right_on="SEQN")
 
+# Recode sex as an indicator for female sex
 dx["Female"] = (dx.RIAGENDR == 2).astype(np.int)
 
+# Recode the ethnic groups
 dx["RIDRETH1"] = dx.RIDRETH1.replace({1: "MA", 2: "OH", 3: "NHW", 4: "NHB", 5: "OR"})
 
+# Drop rows with any missing data in the variables of interest
 dx = dx.dropna()

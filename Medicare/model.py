15d4188e65eec3419288d87c8470bfb16eb6e366
Kerby Shedden <kshedden@umich.edu>
2019-10-28 08:31:29 -0400

    update

--- Medicare/model.py
+++ Medicare/model.py
@@ -7,9 +7,12 @@ df = pd.read_csv("2017_utilization_reduced.csv.gz", dtype={"nppes_provider_zip":
 
 df = df.loc[df.nppes_entity_code=="I", :]
 
+# Include this as a covariate to proxy for practice size
 df["total_npi_cnt"] = df.groupby("npi")["line_srvc_cnt"].transform(np.sum)
 df["log_total_npi_cnt"] = np.log(df.total_npi_cnt)
 
+# Include this as a covariate to proxy for zip code attributes
+# related to population demographics
 df["total_zip_cnt"] = df.groupby("nppes_provider_zip")["line_srvc_cnt"].transform(np.sum)
 df["log_total_zip_cnt"] = np.log(df.total_zip_cnt)
 
@@ -22,30 +25,32 @@ fml = [
 with open("hcpcs_description.json") as fr:
     hcpcs = json.load(fr)
 
-scale_results = []
-icc_results = []
+scale = []
+icc = []
 codes = []
+
+mx = 2
 
 for code, dg in df.groupby("hcpcs_code"):
 
     # Focus on the most common codes
-    if dg.shape[0] < 50000:
+    if dg.shape[0] < 25000:
         continue
 
     codes.append(code)
     print(code)
 
-    scale, icc = [], []
-    for mx in [2]: #0, 1, 2:
+    model = sm.GEE.from_formula(fml[mx], family=sm.families.Poisson(), groups="nppes_provider_zip",
+        cov_struct=sm.cov_struct.Exchangeable(), data=dg)
+    result = model.fit(maxiter=1, first_dep_update=0)
 
-        model = sm.GEE.from_formula(fml[mx], family=sm.families.Poisson(), groups="nppes_provider_zip",
-            cov_struct=sm.cov_struct.Exchangeable(), data=dg)
-        result = model.fit(maxiter=1, first_dep_update=0)
+    # Estimate the scale parameter as if this were a quasi-Poisson model
+    scale.append(np.mean(result.resid_pearson**2))
 
-        # Estimate the scale parameter as if this were a quasi-Poisson model
-        scale.append(np.mean(result.resid_pearson**2))
+    icc.append(result.cov_struct.dep_params)
 
-        icc.append(result.cov_struct.dep_params)
+hd = [hcpcs[c] for c in codes]
 
-    scale_results.append(scale)
-    icc_results.append(icc)
+rslt = pd.DataFrame({"code": codes, "icc": icc, "scale": scale, "description": hd})
+rslt = rslt.sort_values(by="icc")
+rslt.to_csv("model.csv", index=None)

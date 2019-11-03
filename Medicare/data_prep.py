c331b8da129be060bfbc697e8f4ab979e25a4aec
Kerby Shedden <kshedden@umich.edu>
2019-11-03 11:03:22 -0500

    update

--- Medicare/data_prep.py
+++ Medicare/data_prep.py
@@ -5,14 +5,24 @@ link to get the data.
 https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2017.html
 
 After downloading the zip file, unzip it.  Then recompress with gzip
-the file called "Medicare_Provider_Util_Payment_PUF_CY2015.txt".  If
+the file called "Medicare_Provider_Util_Payment_PUF_CY2017.txt".  If
 you decide not to gzip it, you will need to modify the code below.
+
+To obtain additional levels of geographic stratification, you will
+need to obtain crosswalk files from the links below.
+
+https://www.huduser.gov/portal/datasets/usps_crosswalk.html
+
+The zip-tract crosswalk is needed to run the script below.
 """
 
 import pandas as pd
 import numpy as np
 import gzip
 import json
+
+zip_tract = pd.read_excel("ZIP_TRACT_092019.xlsx", dtype={"zip": str})
+zt = {a:b for a,b in zip(zip_tract.zip, zip_tract.tract)}
 
 # The CMS utilization file name
 util_file = "Medicare_Provider_Util_Payment_PUF_CY2017.txt.gz"
@@ -22,7 +32,8 @@ out_file = "2017_utilization_reduced.csv.gz"
 
 usecols = ["npi", "nppes_entity_code", "nppes_provider_zip", "nppes_provider_state",
            "provider_type", "hcpcs_code", "hcpcs_description", "line_srvc_cnt"]
-df = pd.read_csv(util_file, skiprows=[1], delimiter="\t", usecols=usecols)
+df = pd.read_csv(util_file, skiprows=[1], delimiter="\t", usecols=usecols,
+                 dtype={"nppes_provider_zip": str})
 dx = df.groupby(["npi", "hcpcs_code"]).agg({"nppes_entity_code": "first",
                                             "nppes_provider_zip": "first",
                                             "nppes_provider_state": "first",
@@ -30,6 +41,8 @@ dx = df.groupby(["npi", "hcpcs_code"]).agg({"nppes_entity_code": "first",
                                             "hcpcs_description": "first",
                                             "line_srvc_cnt": np.sum})
 dx = dx.reset_index()
+dx = dx.dropna()
+
 dx["line_srvc_cnt"] = dx["line_srvc_cnt"].astype(np.int)
 
 # Save the hcpcs descriptions as a map from hcpcs code to
@@ -41,5 +54,8 @@ h = x.to_dict()["hcpcs_description"]
 with open("hcpcs_description.json", "w") as fh:
     json.dump(h, fh)
 dx = dx.drop("hcpcs_description", axis=1)
+
+# Add tract information
+dx["tract"] = [zt.get(z[0:5], "") for z in dx.nppes_provider_zip]
 
 dx.to_csv(out_file, index=None, compression="gzip")

a856c9e5aa8fd353a3d9c7d0ecb0898e2724c972
Kerby Shedden <kshedden@umich.edu>
2019-11-04 00:01:43 -0500

    update

---
+++ RLMS/lme.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import patsy
+from data_prep import dx
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Mean structure, with and without gender
+fml = "I(np.log2(J10)) ~ C(status) + (bs(year, 5) + bs(age, 5) + bs(educ, 5))*Female + I(np.log2(J8))"
+fml0 = "I(np.log2(J10)) ~ C(status) + bs(year, 5) + bs(age, 5) + bs(educ, 5) + I(np.log2(J8))"
+
+# Random intercepts for subjects
+model1 = sm.MixedLM.from_formula(fml, re_formula="1", groups="idind", data=dx)
+result1 = model1.fit()
+
+# Random intercepts for subjects, and random slopes for age within subjects
+model2 = sm.MixedLM.from_formula(fml, re_formula="1 + age", groups="idind", data=dx)
+result2 = model2.fit()

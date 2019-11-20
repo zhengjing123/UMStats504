f46c89906a371784fff42545b351897aed8df93d
Kerby Shedden <kshedden@umich.edu>
2019-11-20 06:55:14 -0500

    update

---
+++ China/gam.R
@@ -0,0 +1,54 @@
+library(mgcv)
+library(dplyr)
+library(readr)
+library(ggplot2)
+
+df = read_csv("chns.csv.gz")
+
+pdf("gam_plots.pdf", width=8, height=6)
+
+df = df[df$indinc >= 0,]
+df$logindinc = log(1 + df$indinc)
+df$sex = factor(df$female, c(0, 1), c("male", "female"))
+df$urban = factor(df$urban, c(0, 1), c("rural", "urban"))
+
+make_plot = function(vname) {
+
+    df$y = df[[vname]]
+
+    m = gam(y ~ sex*urban + s(age, k=5, by=sex:urban) + s(wave, k=5) + s(educ, k=5) +
+            s(logindinc, k=5), data=df, family=gaussian)
+
+    pd = data.frame(sex=NULL, age=NULL, y=NULL)
+    age = seq(18, 80, length.out=100)
+    for (sex in c("female", "male")) {
+        for (loc in c("rural", "urban")) {
+            dx = df[1:100,]
+            dx$age = age
+            dx$sex = sex
+            dx$urban = loc
+            dx$wave = 2000
+            dx$educ = mean(df$educ)
+            dx$logindinc = mean(df$logindinc)
+            dx = select(dx, wave, sex, urban, logindinc, age, educ)
+            pd = rbind(pd, dx)
+        }
+    }
+    dx$sex = as.factor(dx$sex)
+    dx$urban = as.factor(dx$urban)
+
+    pd$y = predict(m, newdata=pd)
+
+    p = ggplot(pd, aes(x=age, y=y, group=interaction(sex, urban), col=interaction(sex, urban)))
+    p = p + geom_line()
+    p = p + ylab(vname)
+    print(p)
+}
+
+for (vname in c("d3kcal", "d3carbo", "d3fat", "d3protn")) {
+    make_plot(vname)
+}
+
+dev.off()
+
+system("cp gam_plots.pdf ~kshedden")

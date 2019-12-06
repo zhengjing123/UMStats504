e4022c0c36bc698a81f0e3a63dc8be0e62e4dfd6
Kerby Shedden <kshedden@umich.edu>
2019-12-06 00:00:51 -0500

    update

--- Internet/get_files.sh
+++ Internet/get_files.sh
@@ -4,6 +4,8 @@
 base=data.caida.org/datasets/security/telescope-educational/exercises/anon/
 
 # The file names to be retrieved from the CAIDA website
+# The 10-digit numbers are UNIX time stamps for one hour
+# within a day
 readarray files < telescope_files.txt
 
 for f in "${files[@]}"
@@ -11,4 +13,3 @@ do
     rm -f anon/$f
     wget -P anon ${base}$f
 done
-
